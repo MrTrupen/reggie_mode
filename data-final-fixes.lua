4a1181b469fe26d50b85404a67b1240b9b264447
Trupen <MrTrupen@gmail.com>
2024-12-24 17:16:17 +0100

    Refactor magic speed values into variables

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,31 +1,35 @@
 --data-final-fixes.lua
 
+SPEED_MULTIPLAYER = 2
+CRAFTING_MULTIPLAYER = 1 / SPEED_MULTIPLAYER
+
+-- RECIPES
 -- Every recipe craft 2x faster
 for _, recipe in pairs(data.raw.recipe) do
     if recipe.energy_required then
-        recipe.energy_required = recipe.energy_required * 0.5
+        recipe.energy_required = recipe.energy_required * CRAFTING_MULTIPLAYER
     else
-        recipe.energy_required = 0.25
+        recipe.energy_required = 0.5 * CRAFTING_MULTIPLAYER
     end
 end
 
 
--- Belts speeds
--- Every belt speed moves 2x faster
+-- BELTS
+-- Belt speed
 for _, belt in pairs(data.raw["transport-belt"]) do
     if belt.speed then
-        belt.speed = belt.speed * 2
+        belt.speed = belt.speed * SPEED_MULTIPLAYER
     end
 end
--- Every splitter speed moves 2x faster
+-- Spliter speed
 for _, splitter in pairs(data.raw["splitter"]) do
     if splitter.speed then
-        splitter.speed = splitter.speed * 2
+        splitter.speed = splitter.speed * SPEED_MULTIPLAYER
     end
 end
--- Every underground belt speed moves 2x faster
+-- Underground belt speed
 for _, underground in pairs(data.raw["underground-belt"]) do
     if underground.speed then
-        underground.speed = underground.speed * 2
+        underground.speed = underground.speed * SPEED_MULTIPLAYER
     end
 end

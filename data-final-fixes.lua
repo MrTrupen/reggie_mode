937384d496ca154d54bac0aa98de589d84c9af79
Trupen <MrTrupen@gmail.com>
2024-12-24 17:10:48 +0100

    Belts move 2x faster

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -8,3 +8,24 @@ for _, recipe in pairs(data.raw.recipe) do
         recipe.energy_required = 0.25
     end
 end
+
+
+-- Belts speeds
+-- Every belt speed moves 2x faster
+for _, belt in pairs(data.raw["transport-belt"]) do
+    if belt.speed then
+        belt.speed = belt.speed * 2
+    end
+end
+-- Every splitter speed moves 2x faster
+for _, splitter in pairs(data.raw["splitter"]) do
+    if splitter.speed then
+        splitter.speed = splitter.speed * 2
+    end
+end
+-- Every underground belt speed moves 2x faster
+for _, underground in pairs(data.raw["underground-belt"]) do
+    if underground.speed then
+        underground.speed = underground.speed * 2
+    end
+end

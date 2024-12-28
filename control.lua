27f7653e05c5c2b0d205e107b5ff1cd0385434d7
Trupen <MrTrupen@gmail.com>
2024-12-28 14:56:26 +0100

    Mining prod for new forces

--- control.lua
+++ control.lua
@@ -2,3 +2,8 @@
 script.on_init(function()
     game.forces["player"].mining_drill_productivity_bonus = game.forces["player"].mining_drill_productivity_bonus + 1
 end)
+
+-- Boost mining productivity by 100% when a new force is created
+script.on_event(defines.events.on_force_created, function(event)
+    event.force.mining_drill_productivity_bonus = event.force.mining_drill_productivity_bonus + 1
+end)
